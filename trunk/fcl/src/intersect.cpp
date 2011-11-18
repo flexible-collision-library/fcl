a906823f8f30261b93a761b8ec1cd282587ca1d5
isucan <isucan@253336fb-580f-4252-a368-f3cef5a2a82b>
2011-11-18 13:33:04 +0000

    using std::isnan insteasd of isnan to get the code to compile on Mac OS X
    git-svn-id: https://kforge.ros.org/fcl/fcl_ros@43 253336fb-580f-4252-a368-f3cef5a2a82b

--- trunk/fcl/src/intersect.cpp
+++ trunk/fcl/src/intersect.cpp
@@ -1850,7 +1850,7 @@ void TriangleDistance::segPoints(const Vec3f& P, const Vec3f& A, const Vec3f& Q,
 
   // clamp result so t is on the segment P,A
 
-  if((t < 0) || isnan(t)) t = 0; else if(t > 1) t = 1;
+  if((t < 0) || std::isnan(t)) t = 0; else if(t > 1) t = 1;
 
   // find u for point on ray Q,B closest to point at t
 
@@ -1860,13 +1860,13 @@ void TriangleDistance::segPoints(const Vec3f& P, const Vec3f& A, const Vec3f& Q,
   // closest points, otherwise, clamp u, recompute and
   // clamp t
 
-  if((u <= 0) || isnan(u))
+  if((u <= 0) || std::isnan(u))
   {
     Y = Q;
 
     t = A_dot_T / A_dot_A;
 
-    if((t <= 0) || isnan(t))
+    if((t <= 0) || std::isnan(t))
     {
       X = P;
       VEC = Q - P;
@@ -1889,7 +1889,7 @@ void TriangleDistance::segPoints(const Vec3f& P, const Vec3f& A, const Vec3f& Q,
 
     t = (A_dot_B + A_dot_T) / A_dot_A;
 
-    if((t <= 0) || isnan(t))
+    if((t <= 0) || std::isnan(t))
     {
       X = P;
       VEC = Y - P;
@@ -1911,7 +1911,7 @@ void TriangleDistance::segPoints(const Vec3f& P, const Vec3f& A, const Vec3f& Q,
   {
     Y = Q + B * u;
 
-    if((t <= 0) || isnan(t))
+    if((t <= 0) || std::isnan(t))
     {
       X = P;
       TMP = T.cross(B);
