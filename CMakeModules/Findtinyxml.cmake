6eaf2c99dadf3e8afb4975f7c19f7d0fdc632f07
panjia1983 <panjia1983@gmail.com>
2013-08-15 16:24:16 -0700

    fix cmake bug

--- CMakeModules/Findtinyxml.cmake
+++ CMakeModules/Findtinyxml.cmake
@@ -6,11 +6,8 @@
 # TINYXML_INCLUDE_DIR - where to find tinyxml.h, etc.
 # TINYXML_LIBRARIES - List of libraries when using TinyXML.
 #
+INCLUDE( "FindPackageHandleStandardArgs" )
 
-IF( TINYXML_INCLUDE_DIRS)
-# Already in cache, be silent
-SET( TinyXML_FIND_QUIETLY TRUE )
-ENDIF( TINYXML_INCLUDE_DIRS )
 
 FIND_PATH( TINYXML_INCLUDE_DIRS "tinyxml.h"
 PATH_SUFFIXES "tinyxml" )
@@ -21,7 +18,4 @@ PATH_SUFFIXES "tinyxml" )
 
 # handle the QUIETLY and REQUIRED arguments and set TINYXML_FOUND to TRUE if
 # all listed variables are TRUE
-INCLUDE( "FindPackageHandleStandardArgs" )
 FIND_PACKAGE_HANDLE_STANDARD_ARGS( "TinyXML" DEFAULT_MSG TINYXML_INCLUDE_DIRS TINYXML_LIBRARY_DIRS )
-
-MARK_AS_ADVANCED( TINYXML_INCLUDE_DIRS TINYXML_LIBRARY_DIRS )
