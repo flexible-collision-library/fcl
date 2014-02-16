17bb5941d81899a9945212a88c8c1f60ca2c613b
Ioan Sucan <isucan@gmail.com>
2014-02-15 20:36:54 -0800

    bump version

--- CMakeModules/FCLVersion.cmake
+++ CMakeModules/FCLVersion.cmake
@@ -1,8 +1,8 @@
 # set the version in a way CMake can use
 set(FCL_MAJOR_VERSION 0)
-set(FCL_MINOR_VERSION 2)
-set(FCL_PATCH_VERSION 9)
+set(FCL_MINOR_VERSION 3)
+set(FCL_PATCH_VERSION 0)
 set(FCL_VERSION "${FCL_MAJOR_VERSION}.${FCL_MINOR_VERSION}.${FCL_PATCH_VERSION}")
 
 # increment this when we have ABI changes
-set(FCL_ABI_VERSION 3)
+set(FCL_ABI_VERSION 4)
