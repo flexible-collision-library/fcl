b1d5fa3f479afd843dd24390cb60ca2ab46b291a
Jeongseok Lee <jslee02@gmail.com>
2015-03-04 13:40:15 -0500

    Install libboost-dev

--- ci/install_linux.sh
+++ ci/install_linux.sh
@@ -2,4 +2,5 @@ sudo add-apt-repository --yes ppa:libccd-debs/ppa
 sudo apt-get -qq update
 
 sudo apt-get -qq --yes --force-yes install cmake
+sudo apt-get -qq --yes --force-yes install libboost-dev
 sudo apt-get -qq --yes --force-yes install libccd-dev
