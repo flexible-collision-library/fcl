e6ac89477ad4d5cecb82432ac02fe79d4aad018c
Jeongseok Lee <jslee02@gmail.com>
2015-05-03 16:08:37 -0400

    Add tests of building bvh models

--- test/test_fcl_utility.h
+++ test/test_fcl_utility.h
@@ -183,6 +183,8 @@ bool defaultContinuousCollisionFunction(ContinuousCollisionObject* o1, Continuou
 bool defaultContinuousDistanceFunction(ContinuousCollisionObject* o1, ContinuousCollisionObject* o2, void* cdata_, FCL_REAL& dist);
 
 
+std::string getNodeTypeName(NODE_TYPE node_type);
+
 }
 
 #endif
