40d9cf0da38a9a7abcf2e4eeb96c06c0344e0012
Jeongseok Lee <jslee02@gmail.com>
2016-07-26 17:25:05 -0400

    Run travis-ci with eigen

--- ci/install_linux.sh
+++ ci/install_linux.sh
@@ -1,6 +1,7 @@
 sudo add-apt-repository --yes ppa:libccd-debs/ppa
 sudo apt-get -qq update
 
+sudo apt-get -qq --yes --force-yes install libeigen3-dev
 sudo apt-get -qq --yes --force-yes install libccd-dev
 
 # Octomap
--- ci/install_osx.sh
+++ ci/install_osx.sh
@@ -1,5 +1,8 @@
 brew tap homebrew/science
 
+brew update > /dev/null
+
 brew install git
 brew install cmake
+brew install eigen
 brew install libccd
