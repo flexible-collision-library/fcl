aac2c3469884a070646c27ae2e2a753a79b92260
Jeongseok Lee <jslee02@users.noreply.github.com>
2016-08-11 11:42:41 -0400

    Install Octomap for CI test on Mac

--- ci/install_osx.sh
+++ ci/install_osx.sh
@@ -6,3 +6,13 @@ brew install git
 brew install cmake
 brew install eigen
 brew install libccd
+
+# Octomap
+git clone https://github.com/OctoMap/octomap
+cd octomap
+git checkout tags/v1.8.0
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
