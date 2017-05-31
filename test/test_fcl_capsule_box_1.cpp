ef33864748bc7ebef1956d8382a931f9bea6429b
Andrés Valenzuela <andres.valenzuela@tri.global>
2017-05-31 15:07:10 -0400

    Update capsule-box tests to test both solvers

--- test/test_fcl_capsule_box_1.cpp
+++ test/test_fcl_capsule_box_1.cpp
@@ -37,12 +37,13 @@
 #include <gtest/gtest.h>
 
 #include <cmath>
+#include <limits>
 #include "fcl/narrowphase/distance.h"
 #include "fcl/narrowphase/collision.h"
 #include "fcl/narrowphase/collision_object.h"
 
 template <typename S>
-void test_distance_capsule_box()
+void test_distance_capsule_box(fcl::GJKSolverType solver_type, S solver_tolerance, S test_tolerance)
 {
   using CollisionGeometryPtr_t = std::shared_ptr<fcl::CollisionGeometry<S>>;
 
@@ -54,8 +55,9 @@ void test_distance_capsule_box()
   // Enable computation of nearest points
   fcl::DistanceRequest<S> distanceRequest (true);
   fcl::DistanceResult<S> distanceResult;
-  fcl::detail::GJKSolver_libccd<S> gjk_solver;
-  gjk_solver.distance_tolerance = 1e-7;
+
+  distanceRequest.gjk_solver_type = solver_type;
+  distanceRequest.distance_tolerance = solver_tolerance;
 
   fcl::Transform3<S> tf1(fcl::Translation3<S>(fcl::Vector3<S> (3., 0, 0)));
   fcl::Transform3<S> tf2 = fcl::Transform3<S>::Identity();
@@ -63,16 +65,16 @@ void test_distance_capsule_box()
   fcl::CollisionObject<S> box (boxGeometry, tf2);
 
   // test distance
-  fcl::distance (&capsule, &box, &gjk_solver, distanceRequest, distanceResult);
+  fcl::distance (&capsule, &box, distanceRequest, distanceResult);
   // Nearest point on capsule
   fcl::Vector3<S> o1 (distanceResult.nearest_points [0]);
   // Nearest point on box
   fcl::Vector3<S> o2 (distanceResult.nearest_points [1]);
-  EXPECT_NEAR (distanceResult.min_distance, 0.5, 1e-4);
-  EXPECT_NEAR (o1 [0], -2.0, 1e-4);
-  EXPECT_NEAR (o1 [1],  0.0, 1e-4);
-  EXPECT_NEAR (o2 [0],  0.5, 1e-4);
-  EXPECT_NEAR (o2 [1],  0.0, 1e-4);
+  EXPECT_NEAR (distanceResult.min_distance, 0.5, test_tolerance);
+  EXPECT_NEAR (o1 [0], -2.0, test_tolerance);
+  EXPECT_NEAR (o1 [1],  0.0, test_tolerance);
+  EXPECT_NEAR (o2 [0],  0.5, test_tolerance);
+  EXPECT_NEAR (o2 [1],  0.0, test_tolerance);
 
   // Move capsule above box
   tf1 = fcl::Translation3<S>(fcl::Vector3<S> (0., 0., 8.));
@@ -80,19 +82,18 @@ void test_distance_capsule_box()
 
   // test distance
   distanceResult.clear ();
-  fcl::distance (&capsule, &box, &gjk_solver, distanceRequest, distanceResult);
+  fcl::distance (&capsule, &box, distanceRequest, distanceResult);
   o1 = distanceResult.nearest_points [0];
   o2 = distanceResult.nearest_points [1];
 
-  EXPECT_NEAR (distanceResult.min_distance, 2.0, 1e-4);
-  EXPECT_NEAR (o1 [0],  0.0, 1e-4);
-  EXPECT_NEAR (o1 [1],  0.0, 1e-4);
-  EXPECT_NEAR (o1 [2], -4.0, 1e-4);
+  EXPECT_NEAR (distanceResult.min_distance, 2.0, test_tolerance);
+  EXPECT_NEAR (o1 [0],  0.0, test_tolerance);
+  EXPECT_NEAR (o1 [1],  0.0, test_tolerance);
+  EXPECT_NEAR (o1 [2], -4.0, test_tolerance);
 
-  // Disabled broken test lines. Please see #25.
-  EXPECT_NEAR (o2 [0],  0.0, 1e-4);
-  EXPECT_NEAR (o2 [1],  0.0, 1e-4);
-  EXPECT_NEAR (o2 [2],  2.0, 1e-4);
+  EXPECT_NEAR (o2 [0],  0.0, test_tolerance);
+  EXPECT_NEAR (o2 [1],  0.0, test_tolerance);
+  EXPECT_NEAR (o2 [2],  2.0, test_tolerance);
 
   // Rotate capsule around y axis by pi/2 and move it behind box
   tf1.translation() = fcl::Vector3<S>(-10., 0., 0.);
@@ -101,23 +102,27 @@ void test_distance_capsule_box()
 
   // test distance
   distanceResult.clear ();
-  fcl::distance (&capsule, &box, &gjk_solver, distanceRequest, distanceResult);
+  fcl::distance (&capsule, &box, distanceRequest, distanceResult);
   o1 = distanceResult.nearest_points [0];
   o2 = distanceResult.nearest_points [1];
 
-  EXPECT_NEAR (distanceResult.min_distance, 5.5, 1e-4);
-  EXPECT_NEAR (o1 [0],  0.0, 1e-4);
-  EXPECT_NEAR (o1 [1],  0.0, 1e-4);
-  EXPECT_NEAR (o1 [2],  4.0, 1e-4);
-  EXPECT_NEAR (o2 [0], -0.5, 1e-4);
-  EXPECT_NEAR (o2 [1],  0.0, 1e-4);
-  EXPECT_NEAR (o2 [2],  0.0, 1e-4);
+  EXPECT_NEAR (distanceResult.min_distance, 5.5, test_tolerance);
+  EXPECT_NEAR (o1 [0],  0.0, test_tolerance);
+  EXPECT_NEAR (o1 [1],  0.0, test_tolerance);
+  EXPECT_NEAR (o1 [2],  4.0, test_tolerance);
+  EXPECT_NEAR (o2 [0], -0.5, test_tolerance);
+  EXPECT_NEAR (o2 [1],  0.0, test_tolerance);
+  EXPECT_NEAR (o2 [2],  0.0, test_tolerance);
 }
 
-GTEST_TEST(FCL_GEOMETRIC_SHAPES, distance_capsule_box)
+GTEST_TEST(FCL_GEOMETRIC_SHAPES, distance_capsule_box_ccd)
 {
-//  test_distance_capsule_box<float>();
-  test_distance_capsule_box<double>();
+  test_distance_capsule_box<double>(fcl::GJKSolverType::GST_LIBCCD, 1e-6, 1e-4);
+}
+
+GTEST_TEST(FCL_GEOMETRIC_SHAPES, distance_capsule_box_indep)
+{
+  test_distance_capsule_box<double>(fcl::GJKSolverType::GST_INDEP, 1e-8, 1e-4);
 }
 
 //==============================================================================
