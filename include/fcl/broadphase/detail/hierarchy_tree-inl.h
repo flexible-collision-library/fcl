88128e8744abe8ebb43fe9e1e5103bb264d35fd8
Roman Kalashnikov <lunix0x@gmail.com>
2017-10-28 01:36:38 +0300

    Update hierarchy_tree-inl.h

--- include/fcl/broadphase/detail/hierarchy_tree-inl.h
+++ include/fcl/broadphase/detail/hierarchy_tree-inl.h
@@ -1094,7 +1094,8 @@ struct SelectImpl<S, AABB<S>>
     else bv.min_[0] += vel[0];
     if(vel[1] > 0) bv.max_[1] += vel[1];
     else bv.min_[1] += vel[1];
-    bv.max_[2] += vel[2];
+    if(vel[2] > 0) bv.max_[2] += vel[2];
+      else bv.min_[2] += vel[2];
     tree.update(leaf, bv);
     return true;
   }
@@ -1111,7 +1112,8 @@ struct SelectImpl<S, AABB<S>>
     else bv.min_[0] += vel[0];
     if(vel[1] > 0) bv.max_[1] += vel[1];
     else bv.min_[1] += vel[1];
-    bv.max_[2] += vel[2];
+    if(vel[2] > 0) bv.max_[2] += vel[2];
+      else bv.min_[2] += vel[2];
     tree.update(leaf, bv);
     return true;
   }
