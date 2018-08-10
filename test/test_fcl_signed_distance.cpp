c9ccab8b1d87160961e9d8b5cd94f175d8a59d5c
Sean Curtis <sean.curtis@tri.global>
2018-08-10 14:09:30 -0700

    Distance queries return nearest points in the world frame
    The DistanceResult struct documents the near points as being defined in the
    *world* frame. This wasn't true in the code.
    
    1. Correct both gjk solver implementations to put the pose in *world* frame.
    2. Modify sphere-sphere and sphere-capsule custom implementations to do the
       same.
    3. Fix some related typos in documentation.
    4. Update tests to expect points in world frame.

--- test/test_fcl_signed_distance.cpp
+++ test/test_fcl_signed_distance.cpp
@@ -34,6 +34,7 @@
 
 #include <gtest/gtest.h>
 
+#include "eigen_matrix_compare.h"
 #include "fcl/narrowphase/distance.h"
 #include "fcl/narrowphase/detail/traversal/collision_node.h"
 #include "fcl/narrowphase/detail/gjk_solver_libccd.h"
@@ -48,8 +49,10 @@ bool verbose = false;
 template <typename S>
 void test_distance_spheresphere(GJKSolverType solver_type)
 {
-  Sphere<S> s1{20};
-  Sphere<S> s2{10};
+  const S radius_1 = 20;
+  const S radius_2 = 10;
+  Sphere<S> s1{radius_1};
+  Sphere<S> s2{radius_2};
 
   Transform3<S> tf1{Transform3<S>::Identity()};
   Transform3<S> tf2{Transform3<S>::Identity()};
@@ -61,46 +64,126 @@ void test_distance_spheresphere(GJKSolverType solver_type)
 
   DistanceResult<S> result;
 
-  bool res{false};
-
   // Expecting distance to be 10
   result.clear();
   tf2.translation() = Vector3<S>(40, 0, 0);
-  res = distance(&s1, tf1, &s2, tf2, request, result);
-  EXPECT_TRUE(res);
-  EXPECT_TRUE(std::abs(result.min_distance - 10) < 1e-6);
-  EXPECT_TRUE(result.nearest_points[0].isApprox(Vector3<S>(20, 0, 0)));
-  EXPECT_TRUE(result.nearest_points[1].isApprox(Vector3<S>(-10, 0, 0)));
+  distance(&s1, tf1, &s2, tf2, request, result);
+  EXPECT_NEAR(result.min_distance, 10, 1e-6);
+  EXPECT_TRUE(CompareMatrices(result.nearest_points[0], Vector3<S>(20, 0, 0),
+                              request.distance_tolerance));
+  EXPECT_TRUE(CompareMatrices(result.nearest_points[1], Vector3<S>(30, 0, 0),
+                              request.distance_tolerance));
 
-  // Expecting distance to be -5
-  result.clear();
-  tf2.translation() = Vector3<S>(25, 0, 0);
-  res = distance(&s1, tf1, &s2, tf2, request, result);
-
-  EXPECT_TRUE(res);
   // request.distance_tolerance is actually the square of the distance
   // tolerance, namely the difference between distance returned from FCL's EPA
   // implementation and the actual distance, is less than
   // sqrt(request.distance_tolerance).
   const S distance_tolerance = std::sqrt(request.distance_tolerance);
-  EXPECT_NEAR(result.min_distance, -5, distance_tolerance);
 
+  // Expecting distance to be -5
+  result.clear();
+  tf2.translation() = Vector3<S>(25, 0, 0);
+  distance(&s1, tf1, &s2, tf2, request, result);
+  EXPECT_NEAR(result.min_distance, -5, request.distance_tolerance);
+
+  // TODO(JS): Only GST_LIBCCD can compute the pair of nearest points on the
+  // surface of the penetrating spheres.
+  if (solver_type == GST_LIBCCD)
+  {
+    EXPECT_TRUE(CompareMatrices(result.nearest_points[0], Vector3<S>(20, 0, 0),
+                                distance_tolerance));
+    EXPECT_TRUE(CompareMatrices(result.nearest_points[1], Vector3<S>(15, 0, 0),
+                                distance_tolerance));
+  }
+
+  result.clear();
+  tf2.translation() = Vector3<S>(20, 0, 20);
+  distance(&s1, tf1, &s2, tf2, request, result);
+
+  S expected_dist =
+      (tf1.translation() - tf2.translation()).norm() - radius_1 - radius_2;
+  EXPECT_NEAR(result.min_distance, expected_dist, distance_tolerance);
   // TODO(JS): Only GST_LIBCCD can compute the pair of nearest points on the
   // surface of the spheres.
   if (solver_type == GST_LIBCCD)
   {
-    EXPECT_TRUE(result.nearest_points[0].isApprox(Vector3<S>(20, 0, 0),
-                                                  distance_tolerance));
-    EXPECT_TRUE(result.nearest_points[1].isApprox(Vector3<S>(-10, 0, 0),
-                                                  distance_tolerance));
+    Vector3<S> dir = (tf2.translation() - tf1.translation()).normalized();
+    Vector3<S> p0_expected = dir * radius_1;
+    EXPECT_TRUE(CompareMatrices(result.nearest_points[0], p0_expected,
+                                distance_tolerance));
+    Vector3<S> p1_expected = tf2.translation() - dir * radius_2;
+    EXPECT_TRUE(CompareMatrices(result.nearest_points[1], p1_expected,
+                                distance_tolerance));
+  }
+}
+
+template <typename S>
+void test_distance_spherecapsule(GJKSolverType solver_type)
+{
+  Sphere<S> s1{20};
+  Capsule<S> s2{10, 20};
+
+  Transform3<S> tf1{Transform3<S>::Identity()};
+  Transform3<S> tf2{Transform3<S>::Identity()};
+
+  DistanceRequest<S> request;
+  request.enable_signed_distance = true;
+  request.enable_nearest_points = true;
+  request.gjk_solver_type = solver_type;
+
+  DistanceResult<S> result;
+
+  // Expecting distance to be 10
+  result.clear();
+  tf2.translation() = Vector3<S>(40, 0, 0);
+  distance(&s1, tf1, &s2, tf2, request, result);
+  EXPECT_NEAR(result.min_distance, 10, request.distance_tolerance);
+  EXPECT_TRUE(CompareMatrices(result.nearest_points[0], Vector3<S>(20, 0, 0),
+                              request.distance_tolerance));
+  EXPECT_TRUE(CompareMatrices(result.nearest_points[1], Vector3<S>(30, 0, 0),
+                              request.distance_tolerance));
+
+  // Expecting distance to be -5
+  result.clear();
+  tf2.translation() = Vector3<S>(25, 0, 0);
+  distance(&s1, tf1, &s2, tf2, request, result);
+
+  // request.distance_tolerance is actually the square of the distance
+  // tolerance, namely the difference between distance returned from FCL's EPA
+  // implementation and the actual distance, is less than
+  // sqrt(request.distance_tolerance).
+  const S distance_tolerance = std::sqrt(request.distance_tolerance);
+  ASSERT_NEAR(result.min_distance, -5, distance_tolerance);
+  if (solver_type == GST_LIBCCD)
+  {
+    // NOTE: Currently, only GST_LIBCCD computes the pair of nearest points.
+    EXPECT_TRUE(CompareMatrices(result.nearest_points[0], Vector3<S>(20, 0, 0),
+                                distance_tolerance * 100));
+    EXPECT_TRUE(CompareMatrices(result.nearest_points[1], Vector3<S>(15, 0, 0),
+                                distance_tolerance * 100));
   }
 }
 
 //==============================================================================
-GTEST_TEST(FCL_NEGATIVE_DISTANCE, sphere_sphere)
+
+GTEST_TEST(FCL_NEGATIVE_DISTANCE, sphere_sphere_ccd)
 {
   test_distance_spheresphere<double>(GST_LIBCCD);
+}
+
+GTEST_TEST(FCL_NEGATIVE_DISTANCE, sphere_sphere_indep)
+{
   test_distance_spheresphere<double>(GST_INDEP);
+}
+
+GTEST_TEST(FCL_NEGATIVE_DISTANCE, sphere_capsule_ccd)
+{
+  test_distance_spherecapsule<double>(GST_LIBCCD);
+}
+
+GTEST_TEST(FCL_NEGATIVE_DISTANCE, sphere_capsule_indep)
+{
+  test_distance_spherecapsule<double>(GST_INDEP);
 }
 
 //==============================================================================
