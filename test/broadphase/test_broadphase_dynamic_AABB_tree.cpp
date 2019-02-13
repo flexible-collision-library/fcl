8f5294834f71ec0973502505f136f5ce83b678c4
DamrongGuoy <Damrong.Guoy@tri.global>
2019-02-13 12:58:58 -0800

    Check failure on Win32 related to unaligned assertion.

--- test/broadphase/test_broadphase_dynamic_AABB_tree.cpp
+++ test/broadphase/test_broadphase_dynamic_AABB_tree.cpp
@@ -39,10 +39,6 @@
 #include <iostream>
 #include <memory>
 
-// Avoid alignment-assertion failure on Win32. See:
-// http://eigen.tuxfamily.org/dox-devel/group__TopicStlContainers.html
-#include <Eigen/StdVector>
-
 #include <gtest/gtest.h>
 
 #include "fcl/common/types.h"
@@ -50,10 +46,6 @@
 #include "fcl/broadphase/broadphase_dynamic_AABB_tree.h"
 
 using Vector3d = fcl::Vector3d;
-
-// Avoid alignment-assertion failure on Win32. See:
-// http://eigen.tuxfamily.org/dox-devel/group__TopicStlContainers.html
-using PositionList = std::vector<Vector3d, Eigen::aligned_allocator<Vector3d>>;
 
 // Tests repeatability of a dynamic tree of two spheres when we call update()
 // and distance() again and again without changing the poses of the objects.
@@ -76,16 +68,8 @@ GTEST_TEST(DynamicAABBTreeCollisionManager, update) {
   // declared as a const vector here.
   std::vector<fcl::CollisionObjectd> objects{fcl::CollisionObjectd(sphere1),
                                              fcl::CollisionObjectd(sphere2)};
-  // Previously std::vector<Vector3d> caused alignment-assertion failure on
-  // Win32. See:
-  // http://eigen.tuxfamily.org/dox-devel/group__TopicUnalignedArrayAssert.html
-  const PositionList positions {Vector3d(0.1, 0.2, 0.3),
-                                Vector3d(0.11, 0.21, 0.31)};
   fcl::DynamicAABBTreeCollisionManager<double> dynamic_tree;
-  auto o = objects.begin();
-  auto p = positions.begin();
-  for (; o != objects.end(); ++o, ++p) {
-    o->setTranslation(*p);
+  for (auto o = objects.begin(); o != objects.end(); ++o) {
     o->computeAABB();
     dynamic_tree.registerObject(&(*o));
   }
