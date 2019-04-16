963f6885092f6e8aa3e083998349603b8390bbb1
IsaacDore <isaac.dore@omnirobotic.com>
2019-04-16 12:48:13 -0400

    Add own version for static library

---
+++ stdafx.cpp
@@ -0,0 +1,8 @@
+// stdafx.cpp : source file that includes just the standard includes
+// Omni.Fcl.Static.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
