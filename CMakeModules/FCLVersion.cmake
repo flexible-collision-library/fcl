3e9cf34adbba685c04bcea210e310c264bc8c4e0
C. Andy Martin <cam@badger-technologies.com>
2019-06-27 13:51:37 -0400

    Provide catkin package.xml per ROS REP 136
    This provides the least intrusive, but most automated and correct
    method for releasing non-catkin packages through the ROS
    infrastructure.
    
    Instead of specifying the FCL version in CMakeModules/FCLVersion.cmake,
    that module reads the version from package.xml. This provides a single
    point of maintenance for the version number.

--- CMakeModules/FCLVersion.cmake
+++ CMakeModules/FCLVersion.cmake
@@ -1,7 +1,11 @@
 # set the version in a way CMake can use
-set(FCL_MAJOR_VERSION 0)
-set(FCL_MINOR_VERSION 6)
-set(FCL_PATCH_VERSION 0)
+file(READ package.xml PACKAGE_XML)
+string(REGEX MATCH "<version>[0-9]+\\.[0-9]+\\.[0-9]+</version>" DIRTY_VERSION_STRING ${PACKAGE_XML})
+
+string(REGEX REPLACE "^<version>([0-9]+)\\.[0-9]+\\.[0-9]+</version>" "\\1" FCL_MAJOR_VERSION "${DIRTY_VERSION_STRING}")
+string(REGEX REPLACE "^<version>[0-9]+\\.([0-9])+\\.[0-9]+</version>" "\\1" FCL_MINOR_VERSION "${DIRTY_VERSION_STRING}")
+string(REGEX REPLACE "^<version>[0-9]+\\.[0-9]+\\.([0-9]+)</version>" "\\1" FCL_PATCH_VERSION "${DIRTY_VERSION_STRING}")
+
 set(FCL_VERSION "${FCL_MAJOR_VERSION}.${FCL_MINOR_VERSION}.${FCL_PATCH_VERSION}")
 
 # increment this when we have ABI changes
