875baf963f50ec56de67628508f5105ce1d88b6b
C. Andy Martin <cam@badger-technologies.com>
2019-07-26 17:08:04 -0400

    RSS: fix center(), conversion, documentation
    The To of an RSS is the minimum corner of the rectangle, not the
    center. Fix center() to add half the rectangle dimensions along the
    axes to the center. Fix areas of conversion code where To was
    directly being considered the center for RSS. Add a function to set
    the To of an RSS from its center.
    
    Also fix a few bugs in converting to RSS where the computed RSS did
    not contain the entire original BV.
    
    Add tests to cover some of the RSS fit functions.
    
    Correct and improve the documentation, and provide drake-style
    monogram notation aliases for To and axis for clarity.

--- test/test_fcl_math.cpp
+++ test/test_fcl_math.cpp
@@ -39,6 +39,9 @@
 #include "fcl/broadphase/detail/morton.h"
 #include "fcl/config.h"
 #include "fcl/math/bv/AABB.h"
+#include "fcl/math/bv/RSS.h"
+#include "fcl/math/bv/utility.h"
+#include "fcl/math/constants.h"
 
 using namespace fcl;
 
@@ -128,6 +131,205 @@ GTEST_TEST(FCL_MATH, morton)
 //  test_morton<float>();
   test_morton<double>();
 }
+
+// Test fitting an RSS to various number of points
+template <typename S>
+void test_rss_fit()
+{
+  const S epsilon = constants<S>::eps_78();
+  RSS<S> rss;
+  Vector3<S> pn[8];
+  pn[0] << 0, 0, 0;
+  pn[1] << 1, 0, 0;
+  // Check fitting 1 point
+  fit(pn, 1, rss);
+  EXPECT_EQ(rss.center(), pn[0]);
+  EXPECT_EQ(rss.width(), 0.0);
+  EXPECT_EQ(rss.height(), 0.0);
+  EXPECT_EQ(rss.depth(), 0.0);
+  EXPECT_TRUE(rss.axis.isApprox(Matrix3<S>::Identity()));
+  // Check fitting 2 points for each axis
+  fit(pn, 2, rss);
+  EXPECT_TRUE(rss.center().isApprox(Vector3<S>(0.5, 0, 0)));
+  EXPECT_EQ(rss.width(), 1.0);
+  EXPECT_EQ(rss.height(), 0.0);
+  EXPECT_EQ(rss.depth(), 0.0);
+  EXPECT_TRUE(rss.axis.col(0).isApprox(Vector3<S>(1, 0, 0)) ||
+              rss.axis.col(0).isApprox(Vector3<S>(-1, 0, 0)));
+  pn[1] << 0, 1, 0;
+  fit(pn, 2, rss);
+  EXPECT_TRUE(rss.center().isApprox(Vector3<S>(0, 0.5, 0)));
+  EXPECT_EQ(rss.width(), 1.0);
+  EXPECT_EQ(rss.height(), 0.0);
+  EXPECT_EQ(rss.depth(), 0.0);
+  EXPECT_TRUE(rss.axis.col(0).isApprox(Vector3<S>(0, 1, 0)) ||
+              rss.axis.col(0).isApprox(Vector3<S>(0, -1, 0)));
+  pn[1] << 0, 0, 1;
+  fit(pn, 2, rss);
+  EXPECT_TRUE(rss.center().isApprox(Vector3<S>(0, 0, 0.5)));
+  EXPECT_EQ(rss.width(), 1.0);
+  EXPECT_EQ(rss.height(), 0.0);
+  EXPECT_EQ(rss.depth(), 0.0);
+  EXPECT_TRUE(rss.axis.col(0).isApprox(Vector3<S>(0, 0, 1)) ||
+              rss.axis.col(0).isApprox(Vector3<S>(0, 0, -1)));
+  // Check fitting 2 in opposite order
+  pn[0] << 0, 0, 1;
+  pn[1] << 0, 0, 0;
+  fit(pn, 2, rss);
+  EXPECT_TRUE(rss.center().isApprox(Vector3<S>(0, 0, 0.5)));
+  EXPECT_EQ(rss.width(), 1.0);
+  EXPECT_EQ(rss.height(), 0.0);
+  EXPECT_EQ(rss.depth(), 0.0);
+  EXPECT_TRUE(rss.axis.col(0).isApprox(Vector3<S>(0, 0, 1)) ||
+              rss.axis.col(0).isApprox(Vector3<S>(0, 0, -1)));
+  // Check fitting 2 not along an axis
+  pn[0] << -1, 1, 0;
+  pn[1] << 0, 0, 0;
+  fit(pn, 2, rss);
+  EXPECT_TRUE(rss.center().isApprox(Vector3<S>(-0.5, 0.5, 0)));
+  EXPECT_NEAR(rss.width(), sqrt(2.0), epsilon);
+  EXPECT_EQ(rss.height(), 0.0);
+  EXPECT_EQ(rss.depth(), 0.0);
+  EXPECT_TRUE(
+      rss.axis.col(0).isApprox(Vector3<S>(-sqrt(2.0)/2.0, sqrt(2.0)/2.0, 0)) ||
+      rss.axis.col(0).isApprox(Vector3<S>(sqrt(2.0)/2.0, -sqrt(2.0)/2.0, 0)));
+  // Check fitting 3
+  pn[0] << 0, 0, 0;
+  pn[1] << 1, 0, 0;
+  pn[2] << 0, 1, 0;
+  fit(pn, 3, rss);
+  Vector3<S> c3(0.25, 0.25, 0);
+  EXPECT_TRUE(c3.isApprox(rss.center()));
+  EXPECT_NEAR(rss.width(), sqrt(2.0), epsilon);
+  EXPECT_NEAR(rss.height(), sqrt(2.0) / 2.0, epsilon);
+  EXPECT_EQ(rss.depth(), 0.0);
+  // Check fitting 8
+  pn[3] << 0, 0, 1;
+  pn[4] << 1, 0, 0;
+  pn[5] << 1, 0, 1;
+  pn[6] << 1, 1, 0;
+  pn[7] << 1, 1, 1;
+  fit(pn, 8, rss);
+  EXPECT_GE(rss.depth(), 0.5);
+}
+
+// Test convert RSS to other bounding volumes
+template <typename S>
+void test_rss_conversion()
+{
+  const S epsilon = constants<S>::eps_78();
+  RSS<S> rss;
+  Vector3<S> pn[8];
+  AABB<S> aabb;
+  OBB<S> obb;
+
+  pn[0] << 0, 0, 0;
+  pn[1] << 1, 0, 0;
+  pn[2] << 0, 1, 0;
+  pn[3] << 0, 0, 1;
+  pn[4] << 1, 0, 0;
+  pn[5] << 1, 0, 1;
+  pn[6] << 1, 1, 0;
+  pn[7] << 1, 1, 1;
+  fit(pn, 8, rss);
+
+  convertBV(rss, Transform3<S>::Identity(), aabb);
+  EXPECT_GE(aabb.width(), rss.width());
+  EXPECT_GE(aabb.height(), rss.height());
+  EXPECT_GE(aabb.depth(), rss.depth());
+  EXPECT_TRUE(aabb.center().isApprox(rss.center()));
+  convertBV(aabb, Transform3<S>::Identity(), rss);
+  // The resulting RSS must be bigger than the AABB for it to contain it
+  EXPECT_GE(rss.width() - rss.depth() + epsilon, aabb.width());
+  EXPECT_GE(rss.height() - rss.depth() + epsilon, aabb.height());
+  EXPECT_GE(rss.depth(), aabb.depth());
+  EXPECT_TRUE(aabb.center().isApprox(rss.center()));
+  convertBV(rss, Transform3<S>::Identity(), obb);
+  EXPECT_GE(obb.width(), rss.width());
+  EXPECT_GE(obb.height(), rss.height());
+  EXPECT_GE(obb.depth(), rss.depth());
+  EXPECT_TRUE(obb.center().isApprox(rss.center()));
+}
+
+// Test RSS to RSS distance function
+template <typename S>
+void test_rss_distance()
+{
+  const S epsilon = constants<S>::eps_78();
+  Vector3<S> p0[3];
+  Vector3<S> p1[3];
+  RSS<S> rss;
+  RSS<S> rss2;
+  // Test RSS to RSS distance for correctness
+  p0[0] << 1, 1, 1;
+  p0[1] << 1, 1, -1;
+  p0[2] << 0, 1, -1;
+  p1[0] << 1, -1, 1;
+  p1[1] << 1, -1, -1;
+  p1[2] << 0, -1, -1;
+  fit(p0, 3, rss);
+  fit(p1, 3, rss2);
+  EXPECT_NEAR(rss.distance(rss2), 2.0, epsilon);
+  rss.To << 0, 0, 0.5;
+  rss.axis = Matrix3<S>::Identity();
+  rss.l[0] = 1;
+  rss.l[1] = 1;
+  rss.r = 0.5;
+  rss2.To << -1, -1, 2.5;
+  rss2.axis = Matrix3<S>::Identity();
+  rss2.l[0] = 1;
+  rss2.l[1] = 1;
+  rss2.r = 0.5;
+  EXPECT_NEAR(rss.distance(rss2), 1.0, epsilon);
+  rss2.axis << -1, 0, 0,
+                0, -1, 0,
+                0, 0, -1;
+  EXPECT_NEAR(rss.distance(rss2), sqrt(6) - 1.0, epsilon);
+  rss2.To << 0, 0, 2.5;
+  EXPECT_NEAR(rss.distance(rss2), 1.0, epsilon);
+}
+
+// Test setting RSS position
+template <typename S>
+void test_rss_position()
+{
+  // Verify setting To via the center works correctly.
+  RSS<S> rss;
+  rss.l[0] = 1;
+  rss.l[1] = 1;
+  rss.r = 0.5;
+  rss.axis = Matrix3<S>::Identity();
+  rss.setToFromCenter(Vector3<S>(0.5, 0.5, 0.5));
+  EXPECT_TRUE(rss.To.isApprox(Vector3<S>(0.0, 0.0, 0.5)));
+  rss.axis *= -1.0;
+  rss.setToFromCenter(Vector3<S>(-0.5, -0.5, 2.5));
+  EXPECT_TRUE(rss.To.isApprox(Vector3<S>(0.0, 0.0, 2.5)));
+}
+
+GTEST_TEST(FCL_MATH, rss_fit)
+{
+  test_rss_fit<double>();
+}
+
+GTEST_TEST(FCL_MATH, rss_conversion)
+{
+  test_rss_conversion<double>();
+}
+
+GTEST_TEST(FCL_MATH, rss_distance)
+{
+  test_rss_distance<double>();
+}
+
+GTEST_TEST(FCL_MATH, rss_position)
+{
+  test_rss_position<double>();
+}
+
+// TODO test overlap
+// TODO test contain
+// TODO test operator+
+// TODO test size
 
 //==============================================================================
 int main(int argc, char* argv[])
