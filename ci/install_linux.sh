9c9b6be6a52522cac2752ad86233e0a120b1b270
Sean Curtis <sean.curtis@tri.global>
2026-01-27 13:26:18 -0800

    Build octomap with in common release with FCL

--- ci/install_linux.sh
+++ ci/install_linux.sh
@@ -5,13 +5,16 @@ sudo apt-get -qq --yes --force-yes install libeigen3-dev
 sudo apt-get -qq --yes --force-yes install libccd-dev
 
 # Octomap
+DEFAULT_BUILD = "Release"
+BUILD_ARG="${1:-$DEFAULT_BUILD}"
 git clone https://github.com/OctoMap/octomap
 cd octomap
 git checkout tags/v1.10.0
 mkdir build
 cd build
 cmake \
-  -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_OCTOVIS_SUBPROJECT=off \
+  -DCMAKE_BUILD_TYPE=$BUILD_ARG \
   -DCMAKE_POLICY_VERSION_MINIMUM=3.10 \
   ..
 make
