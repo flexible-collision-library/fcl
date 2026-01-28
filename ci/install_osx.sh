de01c8787362f7df7440fc475f29a7c39970979f
Sean Curtis <sean.curtis@tri.global>
2026-01-28 14:24:49 -0800

    Silence all the warnings

--- ci/install_osx.sh
+++ ci/install_osx.sh
@@ -12,8 +12,9 @@ git checkout tags/v1.10.0
 mkdir build
 cd build
 # Note: octomap has some return-type warnings that make AppleClang unhappy.
+# FCL CI should not serve as octomap CI.
 cmake \
-  -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -Wall -Werror -Wextra -Wpedantic -Wno-return-type" \
+  -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -w" \
   -DBUILD_DYNAMICETD3D_SUBPROJECT=off \
   -DBUILD_OCTOVIS_SUBPROJECT=off \
   -DCMAKE_BUILD_TYPE=Release \
